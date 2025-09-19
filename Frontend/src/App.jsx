dae17df678172efca76cc3b83312f21fc6b4e9b6
sujit-27 <sujitshaw029@gmail.com>
2025-09-19 23:49:21 +0530

    Updated codes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
-// import React from 'react';
-// import RoadmapGenerator from './components/RoadmapGenerator';
-// import HomePage from './components/Homepage';
-
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <HomePage />
-//       <RoadmapGenerator />
-//     </div>
-//   );
-// }
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/Homepage';
 import Quizpage from './components/Quizpage';
-=======
-import React from 'react';
->>>>>>> cd27e79f15a5b8eb00c023484bd314c6ccde3bb9
 import RoadmapGenerator from './components/RoadmapGenerator';
 
 export default function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <Routes>
         {/* Homepage */}
@@ -35,10 +18,5 @@ export default function App() {
         <Route path="/roadmap" element={<RoadmapGenerator />} />
       </Routes>
     </Router>
-=======
-    <div className="App">
-      <RoadmapGenerator />
-    </div>
->>>>>>> cd27e79f15a5b8eb00c023484bd314c6ccde3bb9
   );
 }
